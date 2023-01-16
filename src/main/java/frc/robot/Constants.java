0d21b935dbf5fbaddfafe15abf84b0d373be17c8
torif1 <122637128+torif1@users.noreply.github.com>
2023-01-16 12:39:43 +0900

    testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,10 +15,15 @@ package frc.robot;
 public final class Constants {
 
     // Motor Constants
+    //right motors
     public static final int FRONT_LEFT_MOTOR = 1;
     public static final int REAR_LEFT_MOTOR = 2;
     public static final int FRONT_RIGHT_MOTOR = 6;
+
+    //left motors
     public static final int REAR_RIGHT_MOTOR = 5;
+    public static final int idk=10;
+
     public static final int INTAKE_MOTOR = 1;
     public static final int INDEXER_MOTOR = 0;
     public static final int CLIMBER_MOTOR = 21;
