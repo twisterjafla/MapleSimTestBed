a8c8a60a7d0e87387c185143ed6556a772782533
bakedPotatoLord <918780@dpsk12.net>
2023-02-17 22:06:56 -0700

    subClass runCommand

--- src/main/java/frc/robot/commands/ReverseIntakeCommand.java
+++ src/main/java/frc/robot/commands/ReverseIntakeCommand.java
@@ -1,45 +1,27 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
-public class ReverseIntakeCommand extends CommandBase {
 
-  private final IntakeSubsystem m_intakeSubsystem;
-  private final double m_outtakeSpeed;
+public class ReverseIntakeCommand extends RunCommand {
 
-  public ReverseIntakeCommand(IntakeSubsystem outtakeSubsystem, double speed) {
-    m_intakeSubsystem = outtakeSubsystem;
-    m_outtakeSpeed = speed;
-    // Use addRequirements() here to declare subsystem dependencies.
-    // addRequirements(m_intakeSubsystem);
-  }
+  private final IntakeSubsystem intake;
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  public void execute() {
-    m_intakeSubsystem.intakeCargo(m_outtakeSpeed);
-  }
-
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  public boolean isFinished() {
-    return false; // Runs until interrupted
+  public ReverseIntakeCommand(IntakeSubsystem intake, double speed) {
+    super(
+      ()->{
+        intake.intakeCargo(speed);
+      },
+     intake
+     );
+     this.intake = intake;
   }
 
   // Called once after isFinished returns true
   @Override
   public void end(boolean interrupted) {
-    m_intakeSubsystem.intakeCargo(0);
+    super.end(interrupted);
+    intake.intakeCargo(0);
   }
 }
