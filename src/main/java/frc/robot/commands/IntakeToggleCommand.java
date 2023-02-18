14550a241b671d919c7149c24ed55e98d84098fe
bakedPotatoLord <918780@dpsk12.net>
2023-02-17 17:08:38 -0700

    replaced with instantCommand

--- src/main/java/frc/robot/commands/IntakeToggleCommand.java
+++ src/main/java/frc/robot/commands/IntakeToggleCommand.java
@@ -1,37 +1,13 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-//Most of this is coppied from IntakeCommand
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 
-
-public class IntakeToggleCommand extends CommandBase {
-
-    private final IntakeSubsystem m_intakeSubsystem;
-  
-    public IntakeToggleCommand(IntakeSubsystem intakeSubsystem) {
-      //m_intakeSubsystem = bucketSubsystem;
-      m_intakeSubsystem = intakeSubsystem;
-      // Use addRequirements() here to declare subsystem dependencies.
-      //addRequirements(m_bucketSubsystem);
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    public void execute() {
-      m_intakeSubsystem.toggleIntakepiston();
-    }
-  
-    // Make this return true when this Command no longer needs to run execute()
-    @Override
-    public boolean isFinished() {
-      return true; // Runs until interrupted
-    }
-  
-    // Called once after isFinished returns true
-    @Override
-    public void end(boolean interrupted) {
-
-    }
-
+public class IntakeToggleCommand extends InstantCommand {
+  public IntakeToggleCommand(IntakeSubsystem intakeSubsystem) {
+    super(
+      ()->{intakeSubsystem.toggleIntakepiston();},
+      intakeSubsystem
+      );
+  }
 }
