1d62ffc71c49010c799f88635d91e2aa072e762e
bakedPotatoLord <918780@dpsk12.net>
2023-02-18 21:21:27 -0700

    switch to makeDoubleSolenoid syntax

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import com.revrobotics.CANSparkMax;
@@ -13,10 +12,16 @@ public class IntakeSubsystem extends SubsystemBase {
   
   CANSparkMax intakeMotor1 = new CANSparkMax(Constants.INTAKE_MOTOR1,MotorType.kBrushless);
   CANSparkMax intakeMotor2 = new CANSparkMax(Constants.INTAKE_MOTOR2, MotorType.kBrushless);
-  DoubleSolenoid intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.INTAKE_SOLENOID_1, Constants.INTAKE_SOLENOID_2);
+  DoubleSolenoid intakeSolenoid;
   MotorControllerGroup intakeMotors;
   
-  public IntakeSubsystem() {
+  public IntakeSubsystem(Pneumatics pneumatics) {
+
+    intakeSolenoid = pneumatics.makeDoubleSolenoid(
+      Constants.INTAKE_SOLENOID_1, 
+      Constants.INTAKE_SOLENOID_2
+    );
+
     intakeMotor1.setInverted(true);
     intakeMotors = new MotorControllerGroup(intakeMotor1, intakeMotor2);
 
