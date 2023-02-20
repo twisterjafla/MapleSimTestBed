d23b8077297f3294b5ab57fadf3da4c53d589f20
bakedPotatoLord <918780@dpsk12.net>
2023-02-20 00:51:09 -0700

    implicitly set default command

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -8,12 +8,18 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.LimelightCommand;
 
 public class LimelightSubsystem extends SubsystemBase {
     NetworkTable table =NetworkTableInstance.getDefault().getTable("limelight");
 	NetworkTableEntry tx = table.getEntry("tx");
 	NetworkTableEntry ty = table.getEntry("ty");
 	NetworkTableEntry ta = table.getEntry("ta");
+
+    public LimelightSubsystem(){
+        //idk if this is cringe but i didn't feel like puting this in the RobotContainer
+        setDefaultCommand(new LimelightCommand(this));
+    }
 
     public double getX(){
         return tx.getDouble(0);
