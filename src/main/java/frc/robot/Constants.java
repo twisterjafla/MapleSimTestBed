d68d34ad6229874c347b3be18cf2bebaa59ca6b4
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2023-03-25 09:55:51 -0600

    removed 11

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -57,7 +57,7 @@ public final class Constants {
 
         }
         public static final int motor1 = 6;
-        public static final int motor2 = 11;
+        //public static final int motor2 = 11;
         
         //reversed
         public static final double fwdSpeed = 0.4;
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -11,7 +11,7 @@ public class IntakeSubsystem extends SubsystemBase {
   
 
   CANSparkMax intakeMotor1 = new CANSparkMax(Constants.intake.motor1,MotorType.kBrushless);
-  CANSparkMax intakeMotor2 = new CANSparkMax(Constants.intake.motor2, MotorType.kBrushless);
+  //CANSparkMax intakeMotor2 = new CANSparkMax(Constants.intake.motor2, MotorType.kBrushless);
   DoubleSolenoid intakeSolenoid;
 
 
@@ -25,7 +25,7 @@ public class IntakeSubsystem extends SubsystemBase {
     );
 
     intakeMotor1.setInverted(true);
-    intakeMotors = new MotorControllerGroup(intakeMotor1, intakeMotor2);
+    //intakeMotors = new MotorControllerGroup(intakeMotor1, intakeMotor2);
 
     intakeSolenoid.set(DoubleSolenoid.Value.kForward);
   }
@@ -37,7 +37,7 @@ public class IntakeSubsystem extends SubsystemBase {
   public void intakeCargo(double speed) {
     //intakeMotors.set(speed);
     intakeMotor1.set(speed);
-    intakeMotor2.set(-speed);
+    //intakeMotor2.set(-speed);
   }
 
   public void stopMotors(){
