95a7158129f0ad9b1885d7c341879bae73c747b1
bakedPotatoLord <918780@dpsk12.net>
2023-03-27 13:21:16 -0600

    create auto balance constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -67,6 +67,14 @@ public final class Constants {
     public static final class auto{
         public static final double fwdSpeed = -0.6;
         public static final double revSpeed = 0.4;
+
+        public static final class balancePID{
+            public static final double kP = 0.12;
+            public static final double kI = 0;
+            public static final double kD = 0.1;
+            public static final double outputMax = 1;
+            public static final double outputMin = -1;
+        }
     }
 
     public static final int MOVEMENT_JOYSTICK = 0;
