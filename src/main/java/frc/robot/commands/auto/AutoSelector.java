dbef48da0bdc79131465d3f1a82c66df5639753c
= <918780@dpsk12.net>
2023-04-02 19:09:52 -0600

    remove subclassed instantcommand

--- src/main/java/frc/robot/commands/auto/AutoSelector.java
+++ src/main/java/frc/robot/commands/auto/AutoSelector.java
@@ -3,18 +3,15 @@ package frc.robot.commands.auto;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Bucket;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Gyro;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class AutoSelector extends InstantCommand {
+public class AutoSelector {
 
-  CommandScheduler scheduler = CommandScheduler.getInstance();
-
-  final SendableChooser<Command> m_chooser = new SendableChooser<Command>();
+  SendableChooser<Command> m_chooser = new SendableChooser<Command>();
 
   public AutoSelector(DriveSubsystem drive,IntakeSubsystem intake,Bucket bucket,Gyro gyro){
 
