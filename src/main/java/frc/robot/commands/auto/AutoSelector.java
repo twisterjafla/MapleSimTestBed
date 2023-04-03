f54229057fc18807f90df7ba4b1c0f87ec722135
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2023-04-03 16:58:08 -0600

    worked on imports

--- src/main/java/frc/robot/commands/auto/AutoSelector.java
+++ src/main/java/frc/robot/commands/auto/AutoSelector.java
@@ -1,5 +1,10 @@
 package frc.robot.commands.auto;
 
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -18,10 +23,14 @@ public class AutoSelector extends InstantCommand {
 
   public AutoSelector(DriveSubsystem drive,IntakeSubsystem intake,Bucket bucket,Gyro gyro){
 
-    m_chooser.setDefaultOption("Simple Auto", new AutonomousBalanceMobile(drive, intake, bucket, gyro));
-    m_chooser.addOption("Complex Auto", new AutonomousGrab(drive, intake, bucket));
-    m_chooser.addOption("Auto No Mobile", new AutonomousBalanceNoMobile(drive, intake, bucket, gyro));
-    m_chooser.addOption("doNothing", new InstantCommand());
+    PathPlannerTrajectory ComplexAuto = PathPlanner.loadPath("AutonomousBalanceNoMobile", new PathConstraints(4, 3));
+    PathPlannerTrajectory AutoNoMobile = PathPlanner.loadPath("AutonomousBalanceNoMobile", new PathConstraints(4, 3));
+    PathPlannerTrajectory SimpleAuto = PathPlanner.loadPath("AutonomousBalanceMobile", new PathConstraints(4, 3));
+
+    m_chooser.setDefaultOption("Simple Auto", SimpleAuto),
+    m_chooser.addOption("Complex Auto", ComplexAuto),
+    m_chooser.addOption("Auto No Mobile", AutoNoMobile);
+   
 
     SmartDashboard.putData("autos: ", m_chooser);
   }
@@ -30,16 +39,15 @@ public class AutoSelector extends InstantCommand {
     return m_chooser.getSelected();
   }
 
-  PathPlannerTrajectory path = PathPlanner.loadPath("getSelected", new PathConstrains(4, 3));
+ 
 
 // This trajectory can then be passed to a path follower such as a PPSwerveControllerCommand
 // Or the path can be sampled at a given point in time for custom path following
 
 // Sample the state of the path at 1.2 seconds
-PathPlannerState exampleState = (PathPlannerState) examplePath.sample(1.2);
+PathPlannerState exampleState = (PathPlannerState) AutonomousBalanceMobile.sample(1.2);
 
 // Print the velocity at the sampled time
-System.out.println(exampleState.velocityMetersPerSecond);
 
 
 }
