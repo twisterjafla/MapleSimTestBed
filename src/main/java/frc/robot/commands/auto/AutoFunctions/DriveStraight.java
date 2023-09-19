84292854dfe2c424424879452c93057e4b9ef7fd
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2023-09-19 16:18:27 -0600

    works?

--- src/main/java/frc/robot/commands/auto/AutoFunctions/DriveStraight.java
+++ src/main/java/frc/robot/commands/auto/AutoFunctions/DriveStraight.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.auto;
+package frc.robot.commands.auto.AutoFunctions;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
--- src/main/java/frc/robot/commands/auto/AutoRoutines/AutonomousGrab.java
+++ src/main/java/frc/robot/commands/auto/AutoRoutines/AutonomousGrab.java
@@ -2,12 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.auto;
+package frc.robot.commands.auto.AutoRoutines;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.auto.AutoFunctions.*;
 import frc.robot.Constants;
 import frc.robot.subsystems.Bucket;
 import frc.robot.subsystems.DriveBase;
