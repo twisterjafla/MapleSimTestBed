a75643ecf754af5861945bab4001016f18cb4097
theINKJETprinter <150977314+theINKJETprinter@users.noreply.github.com>
2024-02-07 17:10:43 -0700

    Removed Comments

--- src/main/java/frc/robot/commands/SpeakerSaftey.java
+++ src/main/java/frc/robot/commands/SpeakerSaftey.java
@@ -7,15 +7,6 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.SpeakerShooter;
 
 public class SpeakerSaftey extends RunCommand {
-  // setActive setActive;
-
-  /**
-   * Creates a new ArcadeDrive command.
-   *
-   * @param left       The control input for the left side of the drive
-   * @param right      The control input for the right sight of the drive
-   * @param driveSubsystem The driveSubsystem subsystem to drive
-   */
   public SpeakerSaftey(SpeakerShooter importedShooter, DoubleSupplier flySpeed, DoubleSupplier indexSpeed) {
     super(
         ()->{
