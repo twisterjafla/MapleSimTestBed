304071ebab3fee3ccdcbf9a3cd7cfbadeeb9caee
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2024-02-14 19:10:24 -0700

    removed unesasary trajectory

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -196,16 +196,16 @@ public final class Constants {
 
     public static final class Trajectorys{
 
-        public static Trajectory exampleTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(3, 0, new Rotation2d(0)),
-            // Pass config
-            TrajectoryGeneratorObjects.trajectoryConfigurer);
+        // public static Trajectory exampleTrajectory =
+        // TrajectoryGenerator.generateTrajectory(
+        //     // Start at the origin facing the +X direction
+        //     new Pose2d(0, 0, new Rotation2d(0)),
+        //     // Pass through these two interior waypoints, making an 's' curve path
+        //     List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+        //     // End 3 meters straight ahead of where we started, facing forward
+        //     new Pose2d(3, 0, new Rotation2d(0)),
+        //     // Pass config
+        //     TrajectoryGeneratorObjects.trajectoryConfigurer);
 
             
     }
