dff8b054a869ec71e72e1fe7257048b9d6959d44
twisterjafla <122408604+twisterjafla@users.noreply.github.com>
2024-02-15 16:54:19 -0700

    should work but needs tuning

--- src/main/java/frc/robot/controlInitalizer.java
+++ src/main/java/frc/robot/controlInitalizer.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.*;
 import frc.robot.subsystems.DriveBase;
+import frc.robot.subsystems.Gyro;
 import frc.robot.subsystems.ShiftableGearbox;
 
 //object to deal with all ofthe dirty work of multiple control schemes
@@ -14,11 +15,13 @@ public class controlInitalizer {
 
     final ShiftableGearbox gearBox;
 
+    final Gyro gyro;
+
     public controlInitalizer(
-        DriveBase m_driveSubsystem, ShiftableGearbox gearBox){
+        DriveBase m_driveSubsystem, ShiftableGearbox gearBox, Gyro gyro){
         this.gearBox=gearBox;
         this.m_driveSubsystem=m_driveSubsystem;
-
+        this.gyro=gyro;
 
     }
 
@@ -28,7 +31,8 @@ public class controlInitalizer {
         new ArcadeDrive(
               m_driveSubsystem,
               () -> ((-movementController.getLeftTriggerAxis() + movementController.getRightTriggerAxis())),
-              () -> (-movementController.getLeftX() )
+              () -> (-movementController.getLeftX() ),
+              gyro
         ));
 
 
@@ -39,7 +43,8 @@ public class controlInitalizer {
             new ArcadeDrive(
                   m_driveSubsystem,
                   () -> ((-controller.getLeftTriggerAxis() + controller.getRightTriggerAxis())),
-                  () -> (-controller.getLeftX() )
+                  () -> (-controller.getLeftX() ),
+                  gyro
             ));
 
         
@@ -50,7 +55,8 @@ public class controlInitalizer {
             new ArcadeDrive(
                   m_driveSubsystem,
                   () -> ( movementController.getLeftY()),
-                  () -> (-movementController.getRightX())
+                  () -> (-movementController.getRightX()),
+                  gyro
             ));
         movementController.rightTrigger().onTrue(new shiftGears(false, gearBox)).onFalse(new shiftGears(true, gearBox));
 
