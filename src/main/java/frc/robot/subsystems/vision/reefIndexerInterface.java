4bcdda293e361baea7840c58dabb15d8a8d8e443
twisterjafla <122408604+twisterjafla@users.noreply.github.com>
2025-01-28 15:23:15 -0700

    more sim reef indexer fun

--- src/main/java/frc/robot/subsystems/vision/reefIndexerInterface.java
+++ src/main/java/frc/robot/subsystems/vision/reefIndexerInterface.java
@@ -2,8 +2,10 @@ package frc.robot.subsystems.vision;
 
 public interface reefIndexerInterface {
     public boolean[][] getFullReefState();
-    public boolean getIsOpen(int row, int level);
+    public boolean getIsClosed(int row, int level);
     public int getHighestLevelForRow(int row);
-    public int getPointValForItem(int row, int level);
+    public int getPointValForItem(int level);
+    public boolean hasAlgea(int row, int level);
+    public boolean[][] getAlgeaPosits();
     
 }
--- src/main/java/frc/robot/subsystems/vision/simReefIndexer.java
+++ src/main/java/frc/robot/subsystems/vision/simReefIndexer.java
@@ -2,39 +2,78 @@ package frc.robot.subsystems.vision;
 
 import org.ironmaple.simulation.seasonspecific.reefscape2025.ReefscapeReefSimulation;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class simReefIndexer implements reefIndexerInterface{
 
-    public boolean[][] reefState = new boolean[12][4]
 
     @Override
     public boolean[][] getFullReefState() {
-        ReefscapeReefSimulation.getInstance();
+        int[][]base = ReefscapeReefSimulation.getInstance().get().getBranches(DriverStation.getAlliance().get());
+        boolean[][]returnable = new boolean[12][4];
+        int i = 0;
+        
+        for(int[] branch : base){
+            int j = 0;
+            for(int item: branch){
+                returnable[i][j] = item>0;
+                j++;
+            }
+            i++;
+        }
+        return returnable;
     }
 
     @Override
-    public boolean getIsOpen(int row, int level) {
+    public boolean getIsClosed(int row, int level) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getIsOpen'");
+        return ReefscapeReefSimulation.getInstance().get().getBranches(DriverStation.getAlliance().get())[row][level]>0;
     }
 
     @Override
     public int getHighestLevelForRow(int row) {
-        if (getIsOpen(row, 4)){
+        if (!getIsClosed(row, 4)){
             return 4;
         }
-        if (getIsOpen(row, 3)){
+        if (!getIsClosed(row, 3)){
             return 3;
         }
-        if (getIsOpen(row, 2)){
+        if (!getIsClosed(row, 2)){
             return 2;
         }
         return 1;
     }
 
     @Override
-    public int getPointValForItem(int row, int level) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPointValForItem'");
+    public int getPointValForItem(int level) {
+        if (DriverStation.isAutonomous()){
+            switch (level){
+                case 4: return 7;
+                case 3: return 6;
+                case 2: return 4;
+                case 1: return 3;
+            }
+        }
+        else{
+            switch(level){
+                case 4: return 5;
+                case 3: return 4;
+                case 2: return 3;
+                case 1: return 2;
+            }
+        }
+        return 0;
+    }
+
+
+    @Override
+    public boolean hasAlgea(int row, int level){
+        return false;
+    }
+
+    @Override 
+    public boolean[][] getAlgeaPosits(){
+        return new boolean[2][6];
     }
     
 }
